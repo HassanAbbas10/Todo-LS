e7160547bd5f92d1f72cc27516070ce88460cdab
Hassan Abbas <95937687+HassanAbbas10@users.noreply.github.com>
2024-07-30 13:24:50 +0500

    Changes to be committed: 	modified:   src/Pages/Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   }
   return (
     <>
-      <div className="h-full w-full">
+      <div className="h-screen w-full">
         <div>
           <h2 className="text-white mb-10 sm:text-3xl text-2xl flex items-center justify-center font-sans">
             Manage your Todo App
@@ -16,7 +16,7 @@ const Home = () => {
 
           <div className="flex flex-row items-center justify-center">
             <input
-              className="md:w-[50rem] sm:w-[30rem] w-[15rem] sm:mx-10 mx-4  p-3 bg-transparent bg-slate-300 rounded-3xl placeholder:text-slate-500 italic "
+              className=" md:w-[50rem] sm:w-[30rem] w-[15rem] sm:mx-10 mx-4  p-3 bg-transparent bg-slate-300 rounded-3xl placeholder:text-slate-500 italic "
               placeholder="Write Todos"
               // value={todos}
             />
@@ -30,9 +30,16 @@ const Home = () => {
         <div className="mt-10 flex items-center justify-center flex-col">
           
           {/* {"TODO ITEMS"} */}
-          <div className="pl-4 my-3 w-[20rem] md:w-[60rem] rounded-full bg-red-500 border border-green-600">
-            <div className="text-slate-300 p-[0.75rem] flex flex-row">
-              THis is our first todo bois
+          <div className="pl-2 justify-between my-3 w-[22rem] sm:w-[30rem] md:w-[60rem] rounded-2xl bg-red-500 border flex border-indigo-700">
+           
+            <div className="gap-1 text-slate-300 p-[0.75rem] flex flex-row">
+              <input type="checkbox"/>
+              <p>THis is our first todo bois</p>
+            </div>
+            
+            <div className="flex items-center gap-3 mr-4">
+            <button className="p-1.5 px-2 text-sm border rounded-lg bg-white">✅</button>
+            <button className="p-1.5 px-2 text-sm border rounded-lg bg-white">🤞</button>
             </div>
           </div>
           {/* {"TODO ITEMS"} */}
